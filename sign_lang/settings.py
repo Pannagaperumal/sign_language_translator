08d3ecf6df6bfd3bc9f1d798c0b4afb26fb36f2f
Pannaga Perumal <pannagaperumal404@gmail.com>
2023-04-17 23:16:36 +0530

    Update settings.py

--- sign_lang/settings.py
+++ sign_lang/settings.py
@@ -121,11 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STSTICFILES_DIRS=[
-    os.path.join(BASE_DIR,'sign_lang/static')
-]
-django_heroku.settings(locals())
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_DIRS=(os.path.join(BASE_DIR,'sign_lang/static'),)
+
+django_heroku.settings(locals())
